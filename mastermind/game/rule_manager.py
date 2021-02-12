a4c7dbc6f824f8685151e982e2443d741bbd3d53
purnsup <77720252+purnsup@users.noreply.github.com>
2021-02-12 02:03:53 -0700

    director, rule_manager

---
+++ mastermind/game/rule_manager.py
@@ -0,0 +1,38 @@
+
+class Rule_manager:
+    
+    def __init__(self):
+        self.hint = '****'
+    
+    
+    def _get_hint(self, number, guess):
+        """
+        check if the guessed number is in the number
+        return hint: "****"
+        """
+        list_num = []
+        
+        for i in range(4):
+            list_num.append(number[i:i+1])
+        
+        for i in range(4):
+            for j in range(4):
+                if guess[i:i+1] == list_num[j]:
+                    if i == j:
+                        self.hint[i:i+1] = 'x'
+                    else:
+                        self.hint[j:j+1] = 'o'
+        return self.hint
+
+    def _game_over(self, number, guess):
+        """
+        If the guessed number is correct, return False
+        """
+
+        if guess == number:
+            return False
+        
+        return True
+
+
+
