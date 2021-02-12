a4e7dc4e8cb74e541f6baa8d700187d3a6e3efc1
stonks4elon <76183757+stonks4elon@users.noreply.github.com>
2021-02-11 21:09:04 -0700

    created board class and stub functions

---
+++ mastermind/game/board.py
@@ -0,0 +1,33 @@
+from game.roster import Roster
+
+class Board:
+
+    def __init__(self):
+        """
+        The constructor of the Board class
+        """
+        self.roster = Roster()
+
+        pass
+
+    def to_string(self, roster.get_names):
+        """
+        Formats the content to a string so that it can be displayed on the screen without issue
+        Takes the names from the Roster class as arguments
+        :return: string
+        """
+        pass
+
+    def print_guesses(self):
+        """
+        ?
+        :return:
+        """
+        pass
+
+    def print_num_condition(self):
+        """
+        ?
+        :return:
+        """
+        pass
