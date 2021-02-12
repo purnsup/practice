7ca5ac079f41e7a1d41e166a3dfe93b306621b07
jmarsh12 <jeffrey.marsh56@gmail.com>
2021-02-11 22:28:43 -0700

    added some basic code

--- mastermind/game/roster.py
+++ mastermind/game/roster.py
@@ -7,6 +7,8 @@ class Roster:
         """
         The constructor method.
         """
+        self.player1 = ''
+        self.player2 = ''
         pass
 
     def get_name(self):
@@ -14,6 +16,10 @@ class Roster:
         The get_name method simply asks the players to input their names
         and then returns that value to screen.
         """
+        self.player1 = input('Player 1, what is your name? ')
+        self.player2 = input('Player 2 what is your name? ')
+
+        return self.player1 and self.player2
         pass
 
     pass
