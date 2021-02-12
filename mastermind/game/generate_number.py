e8eb4c923e8e28a34a835578f23b445f8704b074
jmarsh12 <jeffrey.marsh56@gmail.com>
2021-02-11 20:51:10 -0700

    stubbed out the methods

---
+++ mastermind/game/generate_number.py
@@ -0,0 +1,33 @@
+import random
+
+
+class GenerateNumber:
+
+    def __init__(self):
+        """
+        The constructor class.
+        """
+        pass
+
+    def _generate_number(self):
+        """
+        The generate_number method is an encapsulated class that simply creates
+        a random number and returns it.
+        """
+        pass
+
+    def get_number(self):
+        """
+        The get_number method calls the generate_number method's return value
+        and sends it to screen.
+        """
+        pass
+
+    def get_guess(self):
+        """
+        The get_guess method prompts the user to input their guess and returns that
+        value to screen.
+        """
+        pass
+
+    pass
