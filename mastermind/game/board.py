a46d74d3eba4a134a491b7ee1e20374a8b0064a6
Craig Wright <craigers.wright@gmail.com>
2021-02-12 15:05:26 -0700

    board class update

--- mastermind/game/board.py
+++ mastermind/game/board.py
@@ -1,12 +1,9 @@
-from game.roster import Roster
-
 class Board:
 
     def __init__(self):
         """
         The constructor of the Board class
         """
-        self.roster = Roster()
         self._spacer = '--------------------\n'
         self._p1_guess = '----'
         self._p2_guess = '----'
@@ -14,13 +11,13 @@ class Board:
 
         pass
 
-    def to_string(self):
+    def to_string(self, p1_name, p2_name):
         """
         Formats the content to a string so that it can be displayed on the screen without issue
         Takes the names from the Roster class as arguments
         :return: string
         """
-        string = self._spacer + "Player " + self.roster.get_P1() + ': ' + self._p1_guess + ', '
+        string = self._spacer + "Player " + p1_name + ': ' + p2_name + ', '
         string +=
 
         pass
